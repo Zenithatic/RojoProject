347228c1f39d6c15a4c5ed8385ef6e071bc4e346
Zenithatic <patmc55555@gmail.com>
2022-10-25 22:21:37 -0400

    door game loop logic

---
+++ src/ServerScriptService/Server/doorloop1.server.lua
@@ -0,0 +1,12 @@
+local RunService = game:GetService("RunService")
+
+-- doors
+local beanDoor1 = game.Workspace.BeansStore.Door.HingeConstraint
+local beanDoor2 = game.Workspace.BeansStore.Door2.HingeConstraint
+
+-- Run code each frame render
+RunService.Heartbeat:Connect(function()
+    -- Update door angles
+    beanDoor1.angle.Value = beanDoor1.CurrentAngle
+    beanDoor2.angle.Value = beanDoor2.CurrentAngle
+end)
